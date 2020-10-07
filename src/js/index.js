5e6e874d470c9a6a7b14f6b7219f596b70d56223
karolwasemann <kw.kwaq@gmail.com>
2020-10-07 14:18:51 +0200

    add api

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,19 @@ import "../scss/main.scss";
 import moment from "moment";
 
 console.log(moment().startOf("day").fromNow());
+
+fetch("https://api.github.com/users/karolwasemann/repos")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    for (let repo of resp) {
+      const { name, html_url } = repo;
+      const repositoryList = document.querySelector(".api__list--js");
+      const myTemplate = `<li>
+      ${name} <a href= '${html_url}' title= 'link do repozytorium ${name} na githubie'> link do githuba </a>
+      </li>`;
+      repositoryList.innerHTML += myTemplate;
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
